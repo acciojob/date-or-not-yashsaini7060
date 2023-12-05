eb799b0251352589bef57e930898a6e7e705bd20
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-12-05 15:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var isDate = function (input) {
   //   write your code here
 	const date = new Date(input);
-	return (date==="Invalid Date" ? false : true);
-	// return date;
+	// alert(date);
+	if (date=="Invalid Date"){
+		return false;
+	}
+	else{
+		return true;
+	} 
 };
-
-
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
